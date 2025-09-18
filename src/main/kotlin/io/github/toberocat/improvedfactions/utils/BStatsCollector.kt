83c82023f7401a2c4ef29e3a0f254346f3c4919e
Etcher <96310503+etcherfx@users.noreply.github.com>
2025-09-17 22:59:57 -0700

    chore: Update bStats ID
    Signed-off-by: Etcher <96310503+etcherfx@users.noreply.github.com>

--- src/main/kotlin/io/github/toberocat/improvedfactions/utils/BStatsCollector.kt
+++ src/main/kotlin/io/github/toberocat/improvedfactions/utils/BStatsCollector.kt
@@ -5,13 +5,9 @@ import io.github.toberocat.improvedfactions.charts.addFactionsChart
 import io.github.toberocat.improvedfactions.charts.addModuleChart
 import org.bstats.bukkit.Metrics
 
-/**
- * Created: 21.07.2023
- * Author: Tobias Madlberger (Tobias)
- */
 class BStatsCollector(plugin: ImprovedFactionsPlugin) {
     init {
-        val metrics = Metrics(plugin, 14810)
+        val metrics = Metrics(plugin, 27305)
         metrics.addModuleChart()
         metrics.addFactionsChart()
     }
